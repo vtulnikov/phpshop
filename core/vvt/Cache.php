ea14dfc955072506dbc09435efefcd5f85e5309d
Vitaliy <vital2k9@gmail.com>
2026-08-31 11:15:02 +0300

    Создали класс Кэширования. В ядре созздаем класс Cache, используем TSingleton. В классе будет 3 метода: set, get, delete. В set будем добавлять данные в кэш по ключу - создавать файл с хешированным названием в папке tmp/cache/file.txt. Метод будет принимать ключ, данные и срок жизни, по умолчанию - 3600 сек. Данные будем формировать через массив с ключом 'data' и еще добавлять ключ 'end_time' равный текущему времени + сроку жизни кэша. Нужно записывать данные в сериализованном виде в файл через file_put_contents. В методе get получаем данные по ключу из файла. Сначала снова создаем файл с хешированным ключом, потом если он существует, десериализуем из него данные и проверяем, что кэш еще действителен, в противном случае удаляем файл. В методе delete делаем то же самое, только просто удаляем файл. В приватном метод getFile формируем путь к файлу, чтобы не дублировать код в двух методах. https://chatgpt.com/g/g-p-6a3d998199f88191a30437903e8a50bd-php-shop/c/6a953372-2eb4-83eb-9654-8413c3027d67

---
+++ core/vvt/Cache.php
@@ -0,0 +1,60 @@
+<?php
+
+declare(strict_types=1);
+
+namespace vvt;
+
+class Cache
+{
+    use TSingleton;
+
+    public function set(string $key, $data, int $lifeTime = 3600): bool
+    {
+        $content['data'] = $data;
+        $content['end_time'] = time() + $lifeTime;
+
+        $hash = hash('sha256', $key) . ".txt";
+        $dir = CACHE . '/tmp/cache/' . substr($hash, 0, 2) . '/';
+        /**
+         * Если директории нет → пытаемся создать 
+         * → если не смогли создать и она всё ещё не существует → возвращаем ошибку.
+         */
+        if (!is_dir($dir)) {
+            if (!mkdir($dir, 0755, true) && !is_dir($dir)) {
+                return false;
+            }
+        }
+        return file_put_contents($dir . $hash, serialize($content), LOCK_EX) !== false;
+    }
+    public function get(string $key): mixed
+    {
+        $file = $this->getFile($key);
+        if (!file_exists($file)) return false;
+
+        $content = unserialize(file_get_contents($file));
+        if(!is_array($content) || !isset($content['end_time'])){
+            unlink($file);
+            return false;
+        }
+
+        if (time() <= $content['end_time']) return $content['data'];
+
+        unlink($file);
+        return false;
+    }
+    public function delete(string $key): bool
+    {
+        $file = $this->getFile($key);
+        if (file_exists($file)) {
+            unlink($file);
+            return true;
+        }
+        return false;
+    }
+    private function getFile(string $key):string
+    {
+        $hash = hash('sha256', $key) . ".txt";
+        $dir = CACHE . '/tmp/cache/' . substr($hash, 0, 2) . '/';
+        return $dir . $hash;
+    }
+}
